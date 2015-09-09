5b57c29782230df6248d2ae2ee24cea07bfa85fb
hesc <hesc@ttyongche.com>
2015-09-09 17:58:39 +0800

    fixed bug

--- app/src/main/java/com/hesc/csdnblog/activity/MainActivity.java
+++ app/src/main/java/com/hesc/csdnblog/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -27,6 +28,8 @@ import org.w3c.dom.Text;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
     @InjectView(R.id.main_blogger)
